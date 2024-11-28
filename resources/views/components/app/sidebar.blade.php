ce3a5e808e20546729259a46ea98c11d8f33827f
AlanFRL <tengounsupercell@gmail.com>
2024-11-28 14:23:37 -0400

    sidebar final

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -168,6 +168,7 @@
                 -->
 
                     <!-- PARA MILENKA -->
+                    <!--
                      <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0">
                         <a class="block text-slate-200 hover:text-white truncate transition duration-150"
                             href="{{ route('ejercicios_hombro') }}">
@@ -182,6 +183,7 @@
                             </div>
                         </a>
                     </li>
+                -->
 
                 </ul>
             </div>
